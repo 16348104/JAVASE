6c3af5ab7047171421a9ea83a3e7850ad606d157
16348104 <16348104@qq.com>
2018-01-30 17:04:04 +0800

    update java ER

---
+++ src/NCR/jbcz/Li1_1.java
@@ -0,0 +1,19 @@
+package NCR.jbcz;
+
+
+/**
+ * Created by zb on 2018/1/30.
+ */
+public class Li1_1 {
+    private int day, month, year;
+
+    public void setDate(d, m, y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    public void showDate() {
+        System.out.println(day + ':' + month + ':' + year + ':');
+    }
+}
