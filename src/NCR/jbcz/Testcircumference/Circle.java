a651996c113008c49505fa59204e012137b42ae2
16348104 <16348104@qq.com>
2018-01-31 17:09:34 +0800

    update javaER

--- src/NCR/jbcz/Testcircumference/Circle.java
+++ src/NCR/jbcz/Testcircumference/Circle.java
@@ -3,13 +3,13 @@ package NCR.jbcz.Testcircumference;
 /**
  * Created by zb on 2018/1/31.
  */
-public class Circle {
+public class Circle {//实例变量调用实例方法
     static double PI = 3.14159265;
     int raduis;
 
-    public double circumference() {
+    public double circumference() {//实例方法
         return 2 * raduis * PI;
     }
-
 }
+
 
---
+++ src/NCR/jbcz/Testcircumference2/Testcircumference.java
@@ -0,0 +1,9 @@
+package NCR.jbcz.Testcircumference2;
+/**
+ * Created by zb on 2018/1/31.
+ */
+public class Testcircumference {
+    public static void main(String[] args) {
+        System.out.println("半径20的周长是："+circumference.circumference(20));
+    }
+}
