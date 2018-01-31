3828ccba4572db6d810407fb0b0317ab5be833eb
16348104 <16348104@qq.com>
2018-01-31 16:24:55 +0800

    update javaER

---
+++ src/NCR/jbcz/Testcircumference/Circle.java
@@ -0,0 +1,15 @@
+package NCR.jbcz.Testcircumference;
+
+/**
+ * Created by zb on 2018/1/31.
+ */
+public class Circle {
+    static double PI = 3.14159265;
+    int raduis;
+
+    public double circumference() {
+        return 2 * raduis * PI;
+    }
+
+}
+
