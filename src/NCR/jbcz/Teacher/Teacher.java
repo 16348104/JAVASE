ad39188c2de6302c010857226d75034a18e5b6c1
16348104 <16348104@qq.com>
2018-02-02 16:35:12 +0800

    update javaER

--- src/NCR/jbcz/Teacher/Teacher.java
+++ src/NCR/jbcz/Teacher/Teacher.java
@@ -20,7 +20,7 @@ public class Teacher {
     public static void main(String[] args) {
         Teacher teacher = new Teacher();//创建对象
         teacher.setTeacher("邓小平", 1970, 8, 9);//调用set方法初始化
-        System.out.println("邓小平的生日：");
+        System.out.print("邓小平的生日：");
         teacher.getBirth().showDate();//调用get方法返回birth对象，再调用对象show方法显示生日；
     }
 
