13b6c952f9aea2948e10229070ab22e2a494d28b
16348104 <16348104@qq.com>
2018-02-02 18:40:42 +0800

    update javaER

---
+++ src/NCR/jbcz/eacher/Date.java
@@ -0,0 +1,20 @@
+package NCR.jbcz.eacher;
+
+/**
+ * Created by zb on 2018/2/2.
+ */
+public class Date {
+    int year;
+    int month;
+    int day;
+
+    public Date(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+    }
+
+    void showDate() {
+        System.out.println(year + ',' + month + ',' + day);
+    }
+}
---
+++ src/NCR/jbcz/eacher/Teacher.java
@@ -0,0 +1,8 @@
+package NCR.jbcz.eacher;
+
+/**
+ * Created by zb on 2018/2/2.
+ */
+public class Teacher {
+
+}
