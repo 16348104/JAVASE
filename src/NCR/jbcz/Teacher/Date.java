67ff4e567c5cef23f9e29cdc655bd606e01fe402
16348104 <16348104@qq.com>
2018-02-02 14:02:19 +0800

    update javaER

--- src/NCR/jbcz/Teacher/Date.java
+++ src/NCR/jbcz/Teacher/Date.java
@@ -4,14 +4,16 @@ package NCR.jbcz.Teacher;
  * Created by zb on 2018/2/2.
  */
 public class Date {   //对象作类成员
-    int year,month,day;
-    Date(int y,int m,int d) {
-        year=y;
-        month=m;
-        day=d;
+    int year, month, day;
+
+    Date(int y, int m, int d) {//构造方法
+        year = y;
+        month = m;
+        day = d;
     }
-    void showDate(){
-        System.out.println(year+','+month+","+day);
+
+    void showDate() {      //显示日期方法
+        System.out.println(year + ',' + month + "," + day);
     }
 
 }
--- src/NCR/jbcz/Teacher/Teacher.java
+++ src/NCR/jbcz/Teacher/Teacher.java
@@ -13,11 +13,14 @@ public class Teacher {
     }
 
     Date getBirth() {
-        return birth;
+        return birth;          //定义get方法作为返回birth变量值
     }
 
     public static void main(String[] args) {
-
+        Teacher teacher = new Teacher();//创建对象
+        teacher.setTeacher("邓小平", 1970, 8, 9);//调用set方法初始化
+        System.out.println("邓小平的生日：");
+        teacher.getBirth().showDate();//调用get方法返回birth对象，再调用对象show方法显示生日；
     }
 
 }
