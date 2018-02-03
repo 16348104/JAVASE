823d8a5047424f976e927b3b636f842e32d4c881
16348104 <16348104@qq.com>
2018-02-03 13:00:06 +0800

    update JavaSE

--- src/NCR/jbcz/eacher/Teacher.java
+++ src/NCR/jbcz/eacher/Teacher.java
@@ -1,7 +1,6 @@
 package NCR.jbcz.eacher;
 
 
-
 /**
  * Created by zb on 2018/2/2.
  * 显示教师生日的应用程序
@@ -10,19 +9,21 @@ public class Teacher {
 
     String name;
     Date birth;
-    void setTeacher(String t1,int y,int m,int d){
+
+    void setTeacher(String t1, int y, int m, int d) {
         name = t1;
-        birth = new Date(y, m,d);
+        birth = new Date(y, m, d);//在set方法中创建Date对象
     }
-    void showTeacher(){
+
+    void showTeacher() {
         System.out.println("名字：" + name);
         System.out.print("生日：");
-        birth.showDate();
+        birth.showDate();//在show方法中调用birth的show方法
     }
 
     public static void main(String[] args) {
-        Teacher teacher=new Teacher();
-        teacher.setTeacher("张闻",1968,6,18);
+        Teacher teacher = new Teacher();
+        teacher.setTeacher("张闻", 1968, 6, 18);
         teacher.showTeacher();
     }
 }
