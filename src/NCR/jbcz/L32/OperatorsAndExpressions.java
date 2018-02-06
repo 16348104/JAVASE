388a785fe9e4b641e09589b94eae7de18f2e22bc
16348104 <16348104@qq.com>
2018-02-07 00:07:48 +0800

    update JavaNCR

--- src/NCR/jbcz/L32/OperatorsAndExpressions.java
+++ src/NCR/jbcz/L32/OperatorsAndExpressions.java
@@ -2,6 +2,8 @@ package NCR.jbcz.L32;
 
 /**
  * Created by dell on 2018/2/6.
+ *  +运算在字符串中的应用:如果第一个操作数是字符串，则Java系统会自动将后续的操作数转换为字符串，然后连接；
+ *  如果第一个操作数不是字符串，则运算结果依据后续的操作数决定，如3+4+5+“abc”的结果为字符串12abc
  */
 public class OperatorsAndExpressions {
 
@@ -15,10 +17,13 @@ public class OperatorsAndExpressions {
         float z = 12.5f, w = 5.5f;
         System.out.println(z + w);//两个操作数都是float型，结果是float型
         System.out.println(z - x);//结果为9.5f
-        int i = 10, j = 3;
-        float m = 213.5f, n = 4.5f;//取模运算在整数和浮点数之间应用
+
+        int i = 10, j = 3;  //取模运算在整数和浮点数之间应用
+        float m = 213.5f, n = 4.5f;
         System.out.println(i % j);
         System.out.println(m % n);
+
+
     }
 
     public static void main(String[] args) {
