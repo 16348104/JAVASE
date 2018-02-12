2f92d31c18b2eae3afdb745cdcb00d3ee7a95e81
16348104 <16348104@QQ.com>
2018-02-12 15:40:59 +0800

    update java二级

---
+++ src/NCR/jbcz/L41/IFStatement.java
@@ -0,0 +1,20 @@
+package NCR.jbcz.L41;
+
+
+
+/**
+ * Created by xdx on 2018/2/12.
+ */
+public class IFStatement {
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 100);
+        System.out.println("The score is:" + x);
+        if (x>=60) {
+            System.out.println("Pass the examination！");
+            System.out.println("Congratulatioins!！");
+            System.out.println("Have a good day!");
+        }
+        System.out.println("Bye Bye!");
+    }
+
+}
