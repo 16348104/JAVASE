452e72f3783a1183f7414d6728f4eefd6acbc814
16348104 <16348104@QQ.com>
2018-02-21 11:25:43 +0800

    update java二级

---
+++ src/NCR/jbcz/L51/TestCar.java
@@ -0,0 +1,18 @@
+package NCR.jbcz.L51;
+
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
+
+/**
+ * Created by xdx on 2018/2/21.
+ */
+public class TestCar {
+    public static void main(String[] args) {
+        Car x = new Car("红旗 AJ", false);
+        Car y = new Car("QQ-WQ", true);
+        System.out.println(x + "等同于" + y + "是：" + x.equals(y));
+        System.out.println("x与y是同样的对吗？" + (x == y));
+        Car z = y;
+        System.out.println(y + "等同于" + z + "是：" + y.equals(z));
+        System.out.println("y与x是同样的对吗？" + (y == z));
+    }
+}
