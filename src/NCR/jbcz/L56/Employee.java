5b00db969dd21c5f7df98b883e513cd696549d7b
16348104 <16348104@QQ.com>
2018-02-23 19:07:39 +0800

    update java二级

--- src/NCR/jbcz/L56/Employee.java
+++ src/NCR/jbcz/L56/Employee.java
@@ -1,7 +1,45 @@
 package NCR.jbcz.L56;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by xdx on 2018/2/23.
  */
 public class Employee {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        this.name = n;
+        this.salary = s;
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        hireDay = calendar.getTime();
+    }
+
+    @Override
+    public boolean equals(Object otherobj) {  //对于object方法覆盖
+        if (this == otherobj) {
+            return true;
+        }
+        if (otherobj == null) {
+            return false;
+        }
+        if (getClass() != otherobj.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) otherobj;
+        return name.equals(other.name) && salary == other.salary && hireDay.equals(other.hireDay);
+    }
+
+    @Override
+    public int hashCode() {  //对于object方法覆盖
+        return 7 * name.hashCode()+11*new Double(salary).hashCode()+13*hireDay.hashCode();
+    }
+
+    @Override
+    public String toString() {    //对于object的方法覆盖
+        return getClass().getName() + "[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
+    }
 }
--- src/NCR/jbcz/L56/ObjectTest.java
+++ src/NCR/jbcz/L56/ObjectTest.java
@@ -5,15 +5,24 @@ package NCR.jbcz.L56;
  */
 public class ObjectTest {
     public static void main(String[] args) {
-        Employee zhang1 = new Employee("张浩", 75000.1987, 12, 15);
+        Employee zhang1 = new Employee("张浩", 75000,1987, 12, 15);
         Employee zhang2 = zhang1;//赋值操作用“==”
-        Employee zhang3 = new Employee("张浩", 75000.1987, 12, 15);
+        Employee zhang3 = new Employee("张浩", 75000,1987, 12, 15);
         Employee bob = new Employee("Bob Brandson", 50000, 1989, 10, 1);
         System.out.println("zhang1==zhang2:" + (zhang1 == zhang2));
         System.out.println("zhang1==zhang3:"+(zhang1==zhang3));//非赋值操作
         System.out.println("zhang1.equals(zhang3):" + zhang1.equals(zhang3));//使用equals
         System.out.println("zhang1.equals(bob):" + zhang1.equals(bob));//使用equals
         System.out.println("bob.to string():" + bob);//覆盖object的to string()
-
+        Manager carl = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        Manager boss = new Manager("Car1 Cracker", 80000, 1987, 12, 15);
+        boss.setBonus(5000);
+        System.out.println("bonus.tostring():"+boss);
+        System.out.println("car1.equals(boss):"+carl.equals(boss));//覆盖
+        System.out.println("zhang1.hashcode():" + zhang1.hashCode());
+        System.out.println("zhang3.hashcode():" + zhang3.hashCode());
+        System.out.println("bob.hashcode():" + bob.hashCode());
+        System.out.println("car1.hashcode():" + carl.hashCode());
+        System.out.println("boss.hashcode():" + boss.hashCode());
     }
 }
