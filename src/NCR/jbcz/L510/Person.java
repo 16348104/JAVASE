71b8cb51a3a6545f9435b23d93e42a1bdff0dee3
16348104 <16348104@qq.com>
2018-02-25 13:56:41 +0800

    update javaER

--- src/NCR/jbcz/L510/Person.java
+++ src/NCR/jbcz/L510/Person.java
@@ -3,5 +3,16 @@ package NCR.jbcz.L510;
 /**
  * Created by zb on 2018/2/25.
  */
-public class Person {
+abstract class Person {//抽象类声明
+    private String name;
+
+    public Person(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public abstract String getDescription();
+    //抽象类方法没有方法体
 }
