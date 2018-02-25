33e3317896eb80cf6d03d8d9cea00ebdbc7ffff6
16348104 <16348104@qq.com>
2018-02-25 10:02:39 +0800

    update javaER

---
+++ src/NCR/jbcz/L58/Light.java
@@ -0,0 +1,23 @@
+package NCR.jbcz.L58;
+
+
+/**
+ * Created by zb on 2018/2/25.
+ */
+public class Light {
+    int wattage;//瓦特数
+    boolean indicator;
+    String location;
+    static int counter;//static变量
+
+    public Light() {
+        wattage = 50;
+        indicator = true;
+        location = "X";
+        counter++;
+    }
+    //static方法
+    public static void writeCount(){
+        System.out.println("Number of lights:" + counter);
+    }
+}
