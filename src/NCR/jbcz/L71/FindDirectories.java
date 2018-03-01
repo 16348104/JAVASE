532efd2fc48d1f2c73d4c3677f931d7298ee3f40
16348104 <16348104@qq.com>
2018-03-01 16:41:21 +0800

    update javaNCR

--- src/NCR/jbcz/L71/FindDirectories.java
+++ src/NCR/jbcz/L71/FindDirectories.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 /**
  * Created by zb on 2018/3/1.
- * 文件流
+ * 打印出用命令行参数指定录井下的所有目录及子目录
  */
 public class FindDirectories {
     public static void main(String[] args) {
@@ -14,10 +14,10 @@ public class FindDirectories {
         }
         try {
             File pathName = new File(args[0]);                      //建立文件（目录）对象
-            String[] fileName = pathName.list();                    //路径文件名数组赋值
+            String[] fileNames = pathName.list();                    //路径文件名数组赋值
             pathName = new File(args[0]);
-            for (int i = 0; i < fileName.length; i++) {
-                File f = new File(pathName.getPath(), fileName[i]);
+            for (int i = 0; i < fileNames.length; i++) {
+                File f = new File(pathName.getPath(), fileNames[i]);
                 if (f.isDirectory()) {
                     System.out.println(f.getCanonicalPath());      //输出规范文件路径
                     main(new String[]{f.getPath()});               //遍历所有子目录
