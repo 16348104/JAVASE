69387fac58e1f6436b7487bba0cc9a37ecc3652a
16348104 <16348104@qq.com>
2018-03-09 16:56:39 +0800

    update javaNCR 对象锁

---
+++ src/NCR/jbcz/L85/Reentrant.java
@@ -0,0 +1,20 @@
+package NCR.jbcz.L85;
+
+/**
+ * Created by zb on 2018/3/9.
+ */
+public class Reentrant {
+    public synchronized void a(){
+        b();
+        System.out.println("here I am,in a()");
+    }
+    public synchronized void b(){
+        System.out.println("here I am.in b()");
+
+    }
+
+    public static void main(String[] args) {
+        Reentrant r=new Reentrant();
+        r.a();
+    }
+}
