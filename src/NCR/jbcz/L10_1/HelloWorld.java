49439122462bfc62f1655e3e8a8f24b53d0618b7
16348104 <16348104@QQ.com>
2018-03-13 23:23:41 +0800

    update java ncr

---
+++ src/NCR/jbcz/L10_1/HelloWorld.java
@@ -0,0 +1,11 @@
+package NCR.jbcz.L10_1;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class HelloWorld extends Applet {
+    public void paint(Graphics graphics) {
+        graphics.drawString("HelloWorld!", 25, 25);
+    }
+
+}
