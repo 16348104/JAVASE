21c9b397805e19898e5e725d5d87e48ae133ba29
16348104 <16348104@qq.com>
2018-03-20 12:08:10 +0800

    update javaNCR

---
+++ src/NCR/jbcz/TEST/Test4.java
@@ -0,0 +1,19 @@
+package NCR.jbcz.TEST;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by zb on 2018/3/20.
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JPanel p = new JPanel();
+        f.setLayout(new BorderLayout());
+        f.getContentPane().add(p, "center");
+        p.setBackground(Color.BLUE);
+        f.setSize(200, 200);
+        f.setVisible(true);
+    }
+}
