f89061eb3665e812502d455a5b1e2f40eab7719a
16348104 <16348104@qq.com>
2018-04-06 18:47:30 +0800

    update JavaNCR

---
+++ src/NCR/jbcz/Base100/exam1.java
@@ -0,0 +1,26 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by dell on 2018/4/6.
+ * 定义一个长度为20的整数数组,将1-20分别赋值给数组元素,计算该数组所有元素之和
+ */
+public class exam1 {
+    public static void main(String[] args) {
+        int sum;
+
+        int arraylist[] = new int[20];
+        for (int i = 0; i < arraylist.length; i++) {
+            arraylist[i] = i + 1;
+        }
+        int pos = 0;
+        while (pos < 20) {
+            if () {
+                sum = sum + arraylist[pos];
+
+            }
+            System.out.println("sum=" + sum);
+        }
+    }
+
+
+}
