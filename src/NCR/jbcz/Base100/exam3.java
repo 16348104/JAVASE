2912c656e2f11fd517fa473400d2881289c98a21
16348104 <16348104@qq.com>
2018-04-07 23:01:29 +0800

    update JavaNCR

--- src/NCR/jbcz/Base100/exam3.java
+++ src/NCR/jbcz/Base100/exam3.java
@@ -2,6 +2,10 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by xdx on 2018/4/7.
+ *
  */
 public class exam3 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/NCR/jbcz/Base100/exam4.java
+++ src/NCR/jbcz/Base100/exam4.java
@@ -2,6 +2,20 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by xdx on 2018/4/7.
+ * 计算出三个数中(1234,456,-987)最大的并输出结果
  */
 public class exam4 {
+    public static void main(String[] args) {
+        int i1 = 1234, i2 = 456, i3 = -987;
+        int MaxValue;
+        MaxValue = max(i1, i2, i3);
+        System.out.println("三个数中最大值是:" + MaxValue);
+    }
+
+    private static int max(int x, int y, int z) {
+        int temp, max_value;
+        temp = x > y ? x : y;
+        max_value = temp > z ? temp : z;
+        return max_value;
+    }
 }
