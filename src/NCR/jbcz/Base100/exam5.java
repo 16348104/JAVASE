dfd8372b5dd6dfb21f624254bdadb3d649f591c1
16348104 <16348104@QQ.com>
2018-04-09 23:25:52 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam5.java
+++ src/NCR/jbcz/Base100/exam5.java
@@ -1,7 +1,16 @@
 package NCR.jbcz.Base100;
-
 /**
- * Created by xdx on 2018/4/9.
+ * Created by xdx on 2018/4/
+ * 读入运行程序时所传入的参数（一个或多个），并将参数依次显示出来，例如运行程序Java exam5 par1 par2,
+ * 则输出par1 par2
  */
 public class exam5 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < args.length) {
+            System.out.println(args[i] + " ");
+            i++;
+        }
+        System.out.println();
+    }
 }
--- src/NCR/jbcz/Base100/exam6.java
+++ src/NCR/jbcz/Base100/exam6.java
@@ -1,7 +1,21 @@
 package NCR.jbcz.Base100;
-
 /**
  * Created by xdx on 2018/4/9.
+ * 把一个字符串传入程序，并与程序中的字符串比较，如果相等则输出“Equal”否则输出 “nequal”
+ * 如运行Java exam6 Welcome,则输出Equal
  */
 public class exam6 {
+    public static void main(String[] args) {
+        String str = new String("Welcome");
+        String temp = null;
+        if (args.length > 0) {
+            temp = args[0];
+        }
+        if (str.equals(temp)) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Unequal");
+            System.out.println();
+        }
+    }
 }
