3810e31d2c8918015de183cc721ebba474c6454b
16348104 <16348104@QQ.com>
2018-04-09 23:38:25 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam7.java
+++ src/NCR/jbcz/Base100/exam7.java
@@ -2,6 +2,12 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by xdx on 2018/4/9.
+ * 输出字符串“Welcome”和表达式a/b=c以及输出\\do something.
  */
 public class exam7 {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("a/b=c");
+        System.out.println("\\\\do something");
+    }
 }
