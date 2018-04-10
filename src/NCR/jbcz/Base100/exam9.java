99da5ba741dafe315b4926af3eba836916aeb903
16348104 <16348104@qq.com>
2018-04-10 16:02:34 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam9.java
+++ src/NCR/jbcz/Base100/exam9.java
@@ -6,5 +6,28 @@ package NCR.jbcz.Base100;
  * 第一个元素和倒数第一个交换，以此类推
  */
 public class exam9 {
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        int i = 0;
+        for (i = 0; i < 20; i++) {
+            arr[i] = (i + 1) * 5;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("==============================================================");
+        int temp;
+        i = 0;
+        int j = 19;
+        while (i <= 9) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (i = 0; i < 19; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 
 }
