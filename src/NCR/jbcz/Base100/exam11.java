b21c5e5b67915713d60bcd8025ac63407446f9d5
16348104 <16348104@qq.com>
2018-04-13 17:41:20 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam11.java
+++ src/NCR/jbcz/Base100/exam11.java
@@ -2,6 +2,14 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by zb on 2018/4/12.
+ * 随机产生若干字母a-z,直到出现字母停止
  */
 public class exam11 {
+    public static void main(String[] args) {
+        char c;
+        do {
+            c = (int) ((Math.random() * 26) + 'A');
+            System.out.println(c + "");
+        } while (c != 'Q');
+    }
 }
