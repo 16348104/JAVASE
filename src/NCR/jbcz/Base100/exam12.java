bece0c3da708b52660052ebf0de2c483336643bf
16348104 <16348104@qq.com>
2018-04-15 01:24:25 +0800

    update Javase ncr

--- src/NCR/jbcz/Base100/exam12.java
+++ src/NCR/jbcz/Base100/exam12.java
@@ -2,6 +2,21 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by zb on 2018/4/12.
+ * 采用递归的方法计算N!(n=8)
  */
 public class exam12 {
+    public static void main(String[] args) {
+        int n=8;
+        System.out.println(n + "!=" + Fac(n));
+
+    }
+
+    private static int Fac(int n) {
+        if () {
+            return 1;
+        } else {
+            ;
+        }
+
+    }
 }
