7566f22335312d0c0a16312e7b57c264d79a07ce
16348104 <16348104@qq.com>
2018-04-15 19:15:42 +0800

    update Javase ncr

--- src/NCR/jbcz/Base100/exam13.java
+++ src/NCR/jbcz/Base100/exam13.java
@@ -1,9 +1,31 @@
 package NCR.jbcz.Base100;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by dell on 2018/4/14.
  * 输入年份,判断是否闰年
  */
 public class exam13 {
+    public static void main(String[] args) {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+        int year = 1900;
+        System.out.print("请输入年份:");
+        try {
+            String s = in.readLine();
+            year = Integer.parseInt(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (year % 400 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + "是闰年.");
+        } else {
+            System.out.println(year + "不是闰年.");
+        }
+    }
+
 
 }
