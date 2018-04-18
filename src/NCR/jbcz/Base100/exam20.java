b40aa2b13b165de4e1155b8791f9e588d3884f1f
16348104 <16348104@qq.com>
2018-04-18 17:42:27 +0800

    update javaNCR

---
+++ src/NCR/jbcz/Base100/exam20.java
@@ -0,0 +1,20 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by zb on 2018/4/18.
+ * 计算二维数组各个元素的和。
+ * 定义了二维数组arr有3行4列共12个元素，程序中采用for循环语句的嵌套来计算数组中各个元素的和，并将结果保存在sum变量中，最后打印输出计算结果。
+ */
+public class exam20 {
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int sum = 0;
+        int i = 0, j = 0;
+        for (i = 0; ) {
+            for () {
+                ;
+                System.out.println("sum=" + sum);
+            }
+        }
+    }
+}
