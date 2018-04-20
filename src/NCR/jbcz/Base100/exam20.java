a493642f045db7cc21da322c636652d0e1a3ce80
16348104 <16348104@qq.com>
2018-04-20 17:39:48 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam20.java
+++ src/NCR/jbcz/Base100/exam20.java
@@ -10,9 +10,9 @@ public class exam20 {
         int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         int sum = 0;
         int i = 0, j = 0;
-        for (i = 0; ) {
-            for () {
-                ;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                sum = sum + arr[i][j];
                 System.out.println("sum=" + sum);
             }
         }
