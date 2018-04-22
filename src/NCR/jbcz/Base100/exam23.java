e6689f89edfa1c52518b15480992c02997683123
16348104 <16348104@QQ.com>
2018-04-23 00:10:38 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam23.java
+++ src/NCR/jbcz/Base100/exam23.java
@@ -5,4 +5,21 @@ package NCR.jbcz.Base100;
  * 用冒泡法对数组元素arr[]={30,1,-9,70}进行排序
  */
 public class exam23 {
+    public static void main(String[] args) {
+        int i,j;
+        int arr[] = {30, 1, -9, 70};
+        int n=;
+        for (i = 0; i < n - 1; i++) {
+            for(j = i+1; j < n; j++) {
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    ;
+                    ;
+                }
+            }
+        }
+        for (i = 0; i < n; i++) {
+            System.out.println(arr[i]+"   ");
+        }
+    }
 }
