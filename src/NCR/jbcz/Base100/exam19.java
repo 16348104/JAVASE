b4cc944976bc95d5eb5e82a087511e35a55e7470
16348104 <16348104@QQ.com>
2018-04-23 00:02:49 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam19.java
+++ src/NCR/jbcz/Base100/exam19.java
@@ -2,6 +2,26 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by xdx on 2018/4/22.
+ * 定义类中带有参数的构造方法和普通方法，并定义类的对象temp，程序中将字符串“Jim”和
+ * 整数19在定义对象时作为构造方法的参数传递给类，然后调用方法printInfo打印传递的变量（如Jim已经有19岁了）
  */
 public class exam19 {
+    String name;
+    int age;
+
+    public exam19(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args) {
+        String name = "Jim";
+        int age = 20;
+        exam19 temp = new exam19(name, age);
+        temp.printInfo();
+
+
+    }
+    private void printInfo() {
+        System.out.println(name + "已经有" + age + "岁了。");
+    }
 }
---
+++ src/NCR/jbcz/Base100/exam23.java
@@ -0,0 +1,8 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/4/22.
+ * 用冒泡法对数组元素arr[]={30,1,-9,70}进行排序
+ */
+public class exam23 {
+}
