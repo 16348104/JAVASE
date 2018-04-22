3f8034cfcdc8adfe1f24ce70bf0e5785a9cebb12
16348104 <16348104@QQ.com>
2018-04-23 00:15:20 +0800

    update java ncr

---
+++ src/NCR/jbcz/Base100/exam25.java
@@ -0,0 +1,18 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/4/23.
+ * 依次输出字符‘4’‘2’和 ‘7’
+ */
+
+public class exam25 {
+    public static void main(String[] args) {
+        char c = '1';
+        ;
+        System.out.println(c);
+        ;
+        System.out.println(c);
+        ;
+        System.out.println(c);
+    }
+}
