e6b14e0eebcb271d81c7cb1281b548115cce554c
16348104 <16348104@qq.com>
2018-04-23 11:28:52 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam23.java
+++ src/NCR/jbcz/Base100/exam23.java
@@ -6,20 +6,20 @@ package NCR.jbcz.Base100;
  */
 public class exam23 {
     public static void main(String[] args) {
-        int i,j;
+        int i, j;
         int arr[] = {30, 1, -9, 70};
-        int n=;
+        int n = arr.length;
         for (i = 0; i < n - 1; i++) {
-            for(j = i+1; j < n; j++) {
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    ;
-                    ;
+            for (j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
         for (i = 0; i < n; i++) {
-            System.out.println(arr[i]+"   ");
+            System.out.println(arr[i] + "   ");
         }
     }
 }
