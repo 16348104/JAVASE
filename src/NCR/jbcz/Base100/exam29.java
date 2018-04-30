4ed4c7d30cc4d3a84ed574a1fad7bd551acc131f
16348104 <16348104@qq.com>
2018-04-30 15:31:23 +0800

    update Javase ncr

---
+++ src/NCR/jbcz/Base100/exam29.java
@@ -0,0 +1,12 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by dell on 2018/4/30.
+ * 随机产生一个1-10间的随机数,根据提示用户可以猜这个随机数.
+ * 例如,程序随机产生的随机数是7,如果输入8,则提示"Try smaller..."并继续提示输入,
+ * 如果输入6,则提示"Try bigger..."并继续提示输入,
+ * 如果输入7,则输出"Great!"并退出程序.
+ */
+public class exam29 {
+
+}
