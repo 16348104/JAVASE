90bb1f610a4e8147ed00085a1f541793112ac843
16348104 <16348104@qq.com>
2018-04-30 18:06:30 +0800

    update Javase ncr

---
+++ src/NCR/jbcz/Base100/exam31.java
@@ -0,0 +1,14 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by dell on 2018/4/30.
+ * 找出字符串"My name is Tom,I come from China."中的大写字母,并打印出来
+ */
+public class exam31 {
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            int i;
+        }
+        System.out.println();
+    }
+}
