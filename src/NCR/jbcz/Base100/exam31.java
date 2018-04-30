6b906edbe00200156e2caf5052a755ab6a5ab0e0
16348104 <16348104@qq.com>
2018-05-01 00:04:07 +0800

    update Javase ncr

--- src/NCR/jbcz/Base100/exam31.java
+++ src/NCR/jbcz/Base100/exam31.java
@@ -1,12 +1,22 @@
 package NCR.jbcz.Base100;
 
+import sun.security.util.Length;
+
 /**
  * Created by dell on 2018/4/30.
  * 找出字符串"My name is Tom,I come from China."中的大写字母,并打印出来
  */
 public class exam31 {
     public static void main(String[] args) {
-        String str="My name is Tom,I come from China.";
-        System.out.println();
+        String str = "My name is Tom,I come from China.";
+        int i = 0;
+        int len = str.length();
+        while (i < len) {
+            char c = str.charAt(i);
+            if (c > 'A' && c < 'Z') {
+                System.out.print(c + "  ");
+            }
+            i++;
+        }
     }
 }
