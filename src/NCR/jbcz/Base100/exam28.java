782f6148799ea9e4f057090277eba458aec459bf
16348104 <16348104@qq.com>
2018-04-30 10:57:58 +0800

    update Javase ncr

--- src/NCR/jbcz/Base100/exam28.java
+++ src/NCR/jbcz/Base100/exam28.java
@@ -2,10 +2,19 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by dell on 2018/4/28.
- * 定义一个Add
+ * 定义一个Add(),用于求两个数的和.
  */
 public class exam28 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int a = 24, b = 34;
+        System.out.println(Add(a, b));
+
+    }
+
+    private static long Add(int a, int b) {
+        int sum = 0;
+        sum = a + b;
+        return sum;
     }
 }
