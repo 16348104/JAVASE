46f9e5d5cb48ae210eaca065b45c3ea44afc3345
16348104 <16348104@QQ.com>
2018-05-08 00:15:44 +0800

    update java ncr

---
+++ src/NCR/jbcz/Base100/exam33.java
@@ -0,0 +1,19 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/5/7.
+ * 提取“China is a great country.”中的前5个字符生成一个新的字符串，并将剩余字符组成另外一个新的字符串，
+ * 最后将两个新的字符串连接输出
+ */
+public class exam33 {
+    public static void main(String[] args) {
+        String str = "China is a great country.";
+        String headstr=null;
+        String trailstr=null;
+        headstr = str.substring(0,5);
+//        trailstr = str.substring(5,str.length()-1);
+        trailstr = str.substring(5);
+        System.out.println(headstr+trailstr);
+    }
+
+}
