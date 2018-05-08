8c50274d632be8a8ab66e396090cb4c7b8606b11
16348104 <16348104@QQ.com>
2018-05-09 03:32:34 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam39.java
+++ src/NCR/jbcz/Base100/exam39.java
@@ -1,7 +1,28 @@
 package NCR.jbcz.Base100;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by dell on 2018/5/8.
+ * 将输入的字符串转换为实数，并打印输出。
  */
 public class exam39 {
+    public static void main(String[] args) {
+        InputStreamReader ir;
+        BufferedReader in;
+        ir = new InputStreamReader(System.in);
+        in = new BufferedReader(ir);
+        double d=0.0;
+        System.out.print("请输入一个实数；");
+        try {
+            String s = in.readLine();
+            d = Double.parseDouble(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("你输入的实数是："+d);
+    }
+
 }
