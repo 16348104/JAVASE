6d7769651905ac7391606e7e16ef96a7f2024554
16348104 <16348104@qq.com>
2018-05-09 17:52:52 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam40.java
+++ src/NCR/jbcz/Base100/exam40.java
@@ -2,7 +2,12 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by dell on 2018/5/8.
+ * 定义一个带有参数的构造方法，并定义一个该类的对象temp，构造方法有两个参数：字符串name和整形age，
+ * 定义对象temp时将字符串“Tom”和整数17传递给构造方法，构造方法将两个参数打印输出（如Tom is 17 years old.）
  */
 public class exam40
 {
+    public static void main(String[] args) {
+
+    }
 }
