46c1796322948bb4c381b33da48084ebef915d03
16348104 <16348104@QQ.com>
2018-05-13 00:38:02 +0800

    update java ncr

---
+++ src/NCR/jbcz/Base100/exam47.java
@@ -0,0 +1,25 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/5/13.
+ * 分别比较两个字符串“A”和“a"是否相等，并比较两个字符“A”和'a'是否相等，并输出比较结果
+ */
+public class exam47 {
+    public static void main(String[] args) {
+        char c1,c2;
+        c1='A';
+        c2='a';
+        String string1 = new String("A"), string2 = new String("a");
+        if () {
+            System.out.println(c1+"equals"+c2);
+        } else {
+            System.out.println(c1+"does not equals"+c2);
+        }
+        if () {
+            System.out.println(string1+"equals"+string2);
+        } else {
+            System.out.println(string1 + "does not equals" + string2);
+        }
+
+    }
+}
