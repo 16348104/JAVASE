3e6375fb8703c706e440cbe6d72a9a10a0159310
16348104 <16348104@qq.com>
2018-05-15 11:08:13 +0800

    update javaNCR

--- src/NCR/jbcz/Base100/exam49.java
+++ src/NCR/jbcz/Base100/exam49.java
@@ -4,14 +4,13 @@ package NCR.jbcz.Base100;
  * Created by xdx on 2018/5/15.
  * 将字符串“Welcome to China!”转化为字符数组，并打印输出。
  */
-public class exam49
-{
+public class exam49 {
     public static void main(String[] args) {
         String str = "Welcome to China!";
         char ch[];
-        ;
-        ;
-        for ()
-            System.out.print(ch [i]);
+        ch = new char[str.length()];
+        ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++)
+            System.out.print(ch[i]);
     }
 }
