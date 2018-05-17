71e957b3136b707e8d1d3d5b75f6ac54fe65f5e8
16348104 <16348104@qq.com>
2018-05-17 23:09:54 +0800

    update Javase ncr

---
+++ src/NCR/jbcz/Base100/exam50.java
@@ -0,0 +1,19 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by dell on 2018/5/17.
+ * 计算半径为2.4的圆的面积
+ */
+public class exam50 {
+    public static void main(String[] args) {
+        double r=2.4;
+        double area = area(r);
+        System.out.println("area=" + area);
+    }
+
+    private static double area(double r) {
+        double area;
+        area = StrictMath.PI * r * r / 2;
+        return area;
+    }
+}
