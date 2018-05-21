959b1e7dd989ce2880aac036bef37060245370dd
16348104 <16348104@qq.com>
2018-05-22 00:51:56 +0800

    update Javase ncr

--- src/NCR/jbcz/Base100/exam56.java
+++ src/NCR/jbcz/Base100/exam56.java
@@ -7,7 +7,7 @@ package NCR.jbcz.Base100;
 public class exam56 {
     public static void main(String[] args) {
         int i, j;
-        for (i = 1; i <= 9; i++) {
+        for (i = 1; i < 10; i++) {
             for (j = 1; j <= i; j++) {
                 System.out.print(i + "x" + j + "=" + i * j + "  ");
             }
