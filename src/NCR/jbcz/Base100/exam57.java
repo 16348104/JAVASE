5b282656b8af9f518c133fd1e7b149bdcd2e0f37
16348104 <16348104@QQ.com>
2018-05-25 00:20:55 +0800

    update java ncr

---
+++ src/NCR/jbcz/Base100/exam57.java
@@ -0,0 +1,22 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/5/25.
+ * 统计score[]={37,89,63,60,59,78,91}中成绩不及格的人数
+ */
+public class exam57 {
+    public static void main(String[] args) {
+    int score[]={37,89,63,60,59,78,91};
+    int sum=0;
+    int i=0;
+        while (i<score.length) {
+            if (score[i]>=60) {
+                i++;
+                continue;
+            }
+            sum++;
+            i++;
+        }
+        System.out.println("below 60 sum:"+sum);
+    }
+}
