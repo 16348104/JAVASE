5bebd1fd57b1309dfa9f9fc3295f90e8390d2dcd
16348104 <16348104@qq.com>
2018-05-26 12:21:45 +0800

    update Javase ncr

---
+++ src/NCR/jbcz/Base100/exam58.java
@@ -0,0 +1,15 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by dell on 2018/5/26.
+ * 判断score[]={89,93,60,59,78,91}中是否所有人的成绩都超过了80分，如果有人没有超过，则显示提示信息“有人未超过80分！”
+ * 否则显示“全部超过80分！”
+ */
+public class exam58 {
+    public static void main(String[] args) {
+    int score[]={89,93,60,59,78,91};
+    int i=0;
+    }
+
+
+}
