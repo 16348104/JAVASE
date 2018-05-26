0bc051c26252c33cff3b8ceb5dde995fa5fc088f
16348104 <16348104@QQ.com>
2018-05-26 17:44:44 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam61.java
+++ src/NCR/jbcz/Base100/exam61.java
@@ -2,9 +2,21 @@ package NCR.jbcz.Base100;
 
 /**
  * Created by xdx on 2018/5/26.
+ * 利用递归的方法求前N个自然数的和（n=10）
  */
 public class exam61 {
     public static void main(String[] args) {
+        int sum = add(10);
+        System.out.println("1+2+3+......+10=" + sum);
+    }
+
+    public static int add(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return add(n - 1) + n;
+        }
 
     }
+
 }
