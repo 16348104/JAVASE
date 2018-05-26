ed980a92552638d97e36c90f68588a692037e3b4
16348104 <16348104@QQ.com>
2018-05-26 17:08:13 +0800

    update java ncr

---
+++ src/NCR/jbcz/Base100/exam59.java
@@ -0,0 +1,18 @@
+package NCR.jbcz.Base100;
+
+/**
+ * Created by xdx on 2018/5/26.
+ * 分别输出字符串a\b、"c\d"和'e/f'
+ */
+public class exam59 {
+    public static void main(String[] args) {
+        String str = new String();
+        str="a\\b";
+        System.out.println(str);
+        str="\"c\\d\"";
+        System.out.println(str);
+        str="'e/f'";
+        System.out.println(str);
+
+    }
+}
