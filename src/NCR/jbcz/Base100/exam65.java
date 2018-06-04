7d6bf1d078b08a240d1cb1e750b22487e6173a39
16348104 <16348104@qq.com>
2018-06-05 00:47:13 +0800

    update Javase ncr

---
+++ src/NCR/jbcz/Base100/exam65.java
@@ -0,0 +1,21 @@
+package NCR.jbcz.Base100;
+
+import javax.lang.model.element.NestingKind;
+
+/**
+ * Created by dell on 2018/6/5.
+ * 将字符串str（"Welcome to China!"）转化为一个字符数组c
+ * 然后将字符数组按数组下标逆序输出
+ */
+public class exam65 {
+    public static void main(String[] args) {
+        String str = "Welcome to China!";
+        char c[];
+        c = new char;
+        ;
+        int i;
+        for (i = 0; i < str.length(); i++) {
+            System.out.println(c[i]);
+        }
+    }
+}
