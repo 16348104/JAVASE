0e0e751ed5dd580ff7528e62033aa4bb1d21f284
16348104 <16348104@qq.com>
2018-07-12 18:32:46 +0800

    update javaNCR

---
+++ src/NCR/jbcz/Base100/exam66.java
@@ -0,0 +1,15 @@
+package NCR.jbcz.Base100;
+
+
+
+/**
+ * Created by zb on 2018/7/12.
+ */
+public class exam66 {
+    public static void main(String[] args) {
+        exam66 fr = new exam66();
+        ;
+        ;
+        fr.setVisible(true);
+    }
+}
