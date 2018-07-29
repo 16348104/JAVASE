d71d7a57b0588691f5d7d145eec2432fb6007a64
16348104 <16348104@QQ.com>
2018-07-29 22:06:19 +0800

    update java ncr

--- src/NCR/jbcz/Base100/exam68.java
+++ src/NCR/jbcz/Base100/exam68.java
@@ -3,6 +3,7 @@ package NCR.jbcz.Base100;
 /**
  * Created by xdx on 2018/7/29.
  * 求1-20间所有奇数的和，并打印输出最终结果
+ * 奇数%2!=0   偶数%2==0
  */
 public class exam68 {
     public static void main(String[] args) {
